d891fb6ac6b503a6247ba1b38cec3aaa622c35ee
Your Nametraopv <traopv99@gmail.com>
2021-01-29 10:56:20 +0700

    create cocoapod

---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Apple on 1/29/21.
+//
+
+import Foundation
+
+public class Service {
+    public init() {}
+    
+    public static func doSomething() -> String {
+        return "My cocoapod!"
+    }
+}
